2283397d37ad8b257bee169ed2dc3db6e16d1216
Adam M <whiskypo082@gmail.com>
2023-11-27 22:40:18 -0600

    Added a shit way to search. Added Mongodb for holding the data to search.

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,40 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly.Extensions.Http;
+using Polly;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () => {
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+    public class AuctionServiceHttpClient
+    {
+        private readonly IConfiguration _config;
+        public readonly HttpClient _httpClient;
+
+        public AuctionServiceHttpClient(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            var lastUpdated = await DB.Find<Item, string>()
+                    .Sort(x => x.Descending(x => x.UpdateAt))
+                    .Project(x => x.UpdateAt.ToString())
+                    .ExecuteFirstAsync();
+
+            return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+        }
+    }
+}
